0e22cd7db100e5fa732a66ccb79fd417c75f28b4
Mehdi Seyedain <mahdi.seyedain@gmail.com>
2021-04-12 13:08:31 +0430

    Update ex_03_03.py

--- ex_03_03.py
+++ ex_03_03.py
@@ -12,19 +12,21 @@ score = input("Enter Score: ")
 try:
     s = float(score)
     
-    if s > 1.0:
-        print("Score must be between 1.0 and 0.0")
-    elif s >= 0.9:
-        print("A")
-    elif s >= 0.8:
-        print("B")
-    elif s >= 0.7:
-        print("C")
-    elif s >= 0.6:
-        print("D")
-    elif s >= 0.0:
-        print("f")
-    else:
-        print("Score must be between 1.0 and 0.0")
 except:
     print("Entered score must be a number!")
+    quit()
+    
+if s > 1.0:
+    print("Score must be between 1.0 and 0.0")
+elif s >= 0.9:
+    print("A")
+elif s >= 0.8:
+    print("B")
+elif s >= 0.7:
+    print("C")
+elif s >= 0.6:
+    print("D")
+elif s >= 0.0:
+    print("f")
+else:
+    print("Score must be between 1.0 and 0.0")
