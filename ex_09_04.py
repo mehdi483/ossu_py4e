9606f8cf7383de9889d438c8093f79f0e326b6c2
Mehdi Seyedain <mahdi.seyedain@gmail.com>
2021-04-15 11:21:42 +0430

    Add files via upload

---
+++ ex_09_04.py
@@ -0,0 +1,26 @@
+name = input("Enter file: ")
+
+try:
+    handle = open(name)
+except:
+    print("Entered file name is not valid")
+    quit()  
+
+words = list()
+senders = dict()
+
+for line in handle:
+    if line.startswith("From "):
+        words = line.split()
+        sender = words[1]
+        senders[sender] = senders.get(sender, 0) + 1
+
+mostSender = None
+mostSenderCount = None
+
+for email,count in senders.items():
+    if mostSender == None or count > mostSenderCount:
+        mostSender = email
+        mostSenderCount = count
+
+print(mostSender, mostSenderCount)
