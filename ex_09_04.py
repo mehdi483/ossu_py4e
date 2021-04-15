81f7500d0268f1304dbab1b744bb54a4fd971404
Mehdi Seyedain <mahdi.seyedain@gmail.com>
2021-04-15 12:31:31 +0430

    Update ex_09_04.py

--- ex_09_04.py
+++ ex_09_04.py
@@ -1,12 +1,13 @@
+# 9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
 name = input("Enter file: ")
 
 try:
     handle = open(name)
 except:
     print("Entered file name is not valid")
-    quit()  
+    quit()
 
-words = list()
 senders = dict()
 
 for line in handle:
