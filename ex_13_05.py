4b4b8bccbab9a19c35a7c45999da3f7bd89f375c
Mehdi Seyedain <mahdi.seyedain@gmail.com>
2021-05-11 14:19:36 +0430

    Add files via upload

---
+++ ex_13_05.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as et, urllib.request
+
+xmlUrl = input("Please enter URL of the xml file: ")
+
+try:
+    req = urllib.request.urlopen(xmlUrl)
+except:
+    print("Entered URL is not valid")
+    quit()
+
+data = req.read().decode()
+
+# print(data)
+
+try:
+    xmlData = et.fromstring(data)
+except:
+    print("Entered URL does not contain a valid xml")
+
+countElements = xmlData.findall('./comments/comment/count')
+
+totalCount = 0
+for countElement in countElements:
+    totalCount += int(countElement.text)
+
+print("Total comments count: ", str(totalCount))
